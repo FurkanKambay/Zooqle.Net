00cf257c791a63e2a0918f730def7c125e893416
Furkan Kambay <furkankambay@outlook.com>
2019-01-25 02:30:36 +0300

    Refactor Poster class

--- Zooqle.Net/Poster.cs
+++ Zooqle.Net/Poster.cs
@@ -4,22 +4,20 @@ namespace Zooqle.Net
 {
     public class Poster
     {
-        internal Poster(int id)
-        {
-            this.id = id;
-            BlurryCover = GetUri(10);
-            Large = GetUri(0);
-            Medium = GetUri(3);
-            Small = GetUri(1);
-            Tiny = GetUri(2);
-        }
+        internal Poster(int id) => this.id = id;
 
         private readonly int id;
-        public readonly Uri BlurryCover;
-        public readonly Uri Large;
-        public readonly Uri Medium;
-        public readonly Uri Small;
-        public readonly Uri Tiny;
+        private Uri tiny;
+        private Uri small;
+        private Uri medium;
+        private Uri large;
+        private Uri blurryCover;
+
+        public Uri Tiny => tiny ?? (tiny = GetUri(2));
+        public Uri Small => small ?? (small = GetUri(1));
+        public Uri Medium => medium ?? (medium = GetUri(3));
+        public Uri Large => large ?? (large = GetUri(0));
+        public Uri BlurryCover => blurryCover ?? (blurryCover = GetUri(10));
 
         private Uri GetUri(int sizeNumber) =>
             new Uri($"{ZooqleClient.ZooqleBaseUrl}img-{id}-{sizeNumber}.jpg");
