a170aa867d78679c2e415174b5cc80e10d41107c
Furkan Kambay <furkankambay@outlook.com>
2019-03-15 11:08:47 +0300

    Reorder members of Poster

--- Zooqle.Net/Poster.cs
+++ Zooqle.Net/Poster.cs
@@ -7,16 +7,20 @@ namespace Zooqle.Net
         internal Poster(int id) => this.id = id;
 
         private readonly int id;
-        private Uri tiny;
-        private Uri small;
-        private Uri medium;
-        private Uri large;
-        private Uri blurryCover;
 
+        private Uri tiny;
         public Uri Tiny => tiny ?? (tiny = GetUri(2));
+
+        private Uri small;
         public Uri Small => small ?? (small = GetUri(1));
+
+        private Uri medium;
         public Uri Medium => medium ?? (medium = GetUri(3));
+
+        private Uri large;
         public Uri Large => large ?? (large = GetUri(0));
+
+        private Uri blurryCover;
         public Uri BlurryCover => blurryCover ?? (blurryCover = GetUri(10));
 
         private Uri GetUri(int sizeNumber) =>
